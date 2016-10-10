50f49e5d6fadcc6792748d8027142b3dd25882c0
Joan Miquel Torres <jmtorres@112ib.com>
2016-10-10 08:21:39 +0200

    Diff wrapper for subvesion:
      * Coloryzed.
      * Ignore tabs and trailing spaces.

---
+++ bin/diffwrap.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Testing:
+# ========
+# echo $0 $1 $2 $3 $4 $5 $6 $7 - $8 $9
+# exit 0
+
+
+# Vimdiff:
+# ========
+## Configure your favorite diff program here.
+#DIFF="/usr/bin/vimdiff"
+#
+## Subversion provides the paths we need as the sixth and seventh
+## parameters.
+#LEFT=${6}
+#RIGHT=${7}
+#
+## Call the diff command (change the following line to make sense for
+## your merge program).
+#$DIFF $LEFT $RIGHT
+#
+## Return an errorcode of 0 if no differences were detected, 1 if some were.
+## Any other errorcode will be treated as fatal.
+
+
+# ColorDiff:
+# ==========
+colordiff -b "${1}" "${2}" "${3}" "${4}" "${5}" "${6}" "${7}"
