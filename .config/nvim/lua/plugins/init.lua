48b97868f18002c97a88dbb86d146bb00dff1f99
Joan Miquel Torres Rigo <jmtorres@112ib.com>
2025-02-18 11:23:14 +0100

    🔧 Disabled neo-tree by now...
      👉 Haven't dug enough in it yet.
    
      ❌ It, somehow, replaced netrw in one of my computers (???).
    
      👍 Let's try later...

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -9,14 +9,14 @@ return {
             vim.cmd.colorscheme "catppuccin"
         end
     }
-    , {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    }
+    -- , {
+    --     "nvim-neo-tree/neo-tree.nvim",
+    --     branch = "v3.x",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    --         "MunifTanjim/nui.nvim",
+    --         -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    --     }
+    -- }
 }
